41219c357b3849ab4d3012da3b4577c5bcebeb99
David Pamerean Budiarto <davidpamereanb@gmail.com>
2022-03-05 14:45:30 +0700

    feat: add db & auth packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import SettingsContext from "contexts/SettingsContext";
+import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SettingsContext>
+      <NextNProgress />
       <Component {...pageProps} />
     </SettingsContext>
   );
