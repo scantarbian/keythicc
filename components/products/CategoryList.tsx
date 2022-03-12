225e4d6aaad05d5c6b0b9043ba7f467922c107e8
David Pamerean Budiarto <davidpamereanb@gmail.com>
2022-03-12 15:46:24 +0700

    feat: properly style category list

--- components/products/CategoryList.tsx
+++ components/products/CategoryList.tsx
@@ -14,12 +14,19 @@ const CategoryList = ({
   className,
 }: CategoryProps) => {
   return (
-    <div className={`flex flex-col text-white gap-4 ${className}`}>
+    <div
+      className={`flex flex-col text-white gap-4 items-start px-8 ${className}`}
+    >
+      <span className="text-xl font-bold border-b-2 w-full pb-2 ">
+        CATEGORIES
+      </span>
       {categories.map((category, index) => (
         <button
           key={index}
           onClick={() => onClick(index)}
-          className={`${active === index ? "font-bold" : ""}`}
+          className={`hover:text-orange-400 ${
+            active === index ? "font-bold text-orange-400" : "font-light"
+          }`}
         >
           <span>{category.name}</span>
         </button>
