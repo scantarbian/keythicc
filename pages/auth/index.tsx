678a5c26bcc4d2b87eb4b5b152c01109bd650c0c
David Pamerean Budiarto <davidpamereanb@gmail.com>
2022-03-20 00:48:56 +0700

    fix: links on index and register, move signup to /auth/register

--- pages/auth/index.tsx
+++ pages/auth/index.tsx
@@ -11,7 +11,6 @@ import BottomImage from "public/images/bgbawah.png";
 import SeePass from "public/images/SeePass.svg";
 import Assemble from "public/images/assembleimg.svg";
 
-
 type Inputs = {
   email: string;
   password: string;
@@ -49,7 +48,9 @@ const Login: NextPage = () => {
         <div className="bg-white shadow-2xl flex">
           <div className="w-1/2 m-5 p-10 mx-10 flex flex-col">
             <Link href="/">
-              <h1 className="text-3xl font-bold text-black-500 text-left mb-24">KeyThicc</h1>
+              <h1 className="text-3xl font-bold text-black-500 text-left mb-24">
+                KeyThicc
+              </h1>
             </Link>
             <form
               className="grid grid-col items-center mt-12 gap-0 h-full"
@@ -58,20 +59,30 @@ const Login: NextPage = () => {
               <h2 className="text-3xl font-bold text-black-500 text-left mb-12 pr-20">
                 Logout the past, Login <br></br> to the new!
               </h2>
-              <p className="text-sm font-bold text-black-500 text-left mb-2">E-mail Address</p>
+              <p className="text-sm font-bold text-black-500 text-left mb-2">
+                E-mail Address
+              </p>
               <input
                 type="email"
                 placeholder=""
                 className="bg-white border-2 border-gray-300 rounded text-xl p-2 w-full"
                 {...register("email", {
                   required: true,
-                })} 
+                })}
               />
               <div className="flex justify-between gap-8 mb-2 mt-6">
                 <div className="items-center gap-2">
-                  <span className="text-sm font-bold text-black-500 text-left">Password</span>
-                </div> <a className="text-sm font-normal text-blue-500 forgot" href="#">Forgot Password?</a>
-              </div>       
+                  <span className="text-sm font-bold text-black-500 text-left">
+                    Password
+                  </span>
+                </div>{" "}
+                <a
+                  className="text-sm font-normal text-blue-500 forgot"
+                  href="#"
+                >
+                  Forgot Password?
+                </a>
+              </div>
               <input
                 type="password"
                 placeholder=""
@@ -81,22 +92,33 @@ const Login: NextPage = () => {
                 })}
               />
 
-              <label className="flex items-center text-sm my-8 text-gray-500"><input type="checkbox" name="Remember" className="border-2 rounded border-gray-300 mr-2"></input>Remember my password</label>     
-              
+              <label className="flex items-center text-sm my-8 text-gray-500">
+                <input
+                  type="checkbox"
+                  name="Remember"
+                  className="border-2 rounded border-gray-300 mr-2"
+                ></input>
+                Remember my password
+              </label>
+
               <div className="flex mt-4">
                 <button
                   type="submit"
                   className="border-2 border-black-500 rounded text-white bg-black text-xl px-20 py-2 font-semibold hover:bg-green-500"
-                > Login
+                >
+                  Login
                 </button>
               </div>
-              
-              <div className="flex justify-left gap-2 mt-12 mb-32">           
-                <span className="text-sm font-normal text-black-500 text-left mb-2">Don&apos;t have an account?</span>
-                <a className="text-sm font-normal text-blue-500" href="#">Sign Up</a>
-              </div>          
+
+              <div className="flex justify-left gap-2 mt-12 mb-32">
+                <span className="text-sm font-normal text-black-500 text-left mb-2">
+                  Don&apos;t have an account?
+                </span>
+                <Link href="/auth/register">
+                  <a className="text-sm font-normal text-blue-500">Sign Up</a>
+                </Link>
+              </div>
             </form>
-            
           </div>
           <div className="w-1/2 bg-black text-white p-32">
             <div className="flex w-3/5 h-3/5 mx-auto">
--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -11,8 +11,7 @@ import SeePass from "public/images/SeePass.svg";
 import Assemble from "public/images/assembleimg.svg";
 import sidekeyboard from "public/images/sidekeyboard.svg";
 
-
-export default function Home() {
+export default function Register() {
   return (
     <>
       <Head>
@@ -24,61 +23,91 @@ export default function Home() {
         <div className="bg-white shadow-2xl flex">
           <div className="w-1/2 m-5 p-10 mx-10 flex flex-col">
             <Link href="/">
-              <h1 className="text-3xl font-bold text-black-500 text-left mb-24">KeyThicc</h1>
+              <h1 className="text-3xl font-bold text-black-500 text-left mb-24">
+                KeyThicc
+              </h1>
             </Link>
-            <form
-              className="grid grid-col items-center mt-12 gap-0 h-full"
-            >
+            <form className="grid grid-col items-center mt-12 gap-0 h-full">
               <h2 className="text-3xl font-bold text-black-500 text-left mb-12 pr-20">
-                Keep your head in the <br></br> clouds and your hands <br></br> on the keyboard
+                Keep your head in the <br></br> clouds and your hands <br></br>{" "}
+                on the keyboard
               </h2>
 
-              <p className="text-sm font-bold text-black-500 text-left mb-2">Full Name</p>
+              <p className="text-sm font-bold text-black-500 text-left mb-2">
+                Full Name
+              </p>
               <input
                 type="Name"
                 placeholder=""
                 className="bg-white border-2 border-gray-300 rounded text-xl p-2 w-full"
               />
 
-              <p className="text-sm font-bold text-black-500 text-left mb-2 mt-6">E-mail Address</p>
+              <p className="text-sm font-bold text-black-500 text-left mb-2 mt-6">
+                E-mail Address
+              </p>
               <input
                 type="email"
                 placeholder=""
                 className="bg-white border-2 border-gray-300 rounded text-xl p-2 w-full"
               />
-              <p className="text-sm font-bold text-black-500 text-left mb-2 mt-6">Password</p>
+              <p className="text-sm font-bold text-black-500 text-left mb-2 mt-6">
+                Password
+              </p>
               <input
                 type="password"
                 placeholder=""
                 className="bg-white border-2 border-gray-300 rounded text-xl p-2 w-full"
               />
 
-              <p className="text-sm font-bold text-black-500 text-left mb-2 mt-6">Confirm Password</p>
+              <p className="text-sm font-bold text-black-500 text-left mb-2 mt-6">
+                Confirm Password
+              </p>
               <input
                 type="password"
                 placeholder=""
                 className="bg-white border-2 border-gray-300 rounded text-xl p-2 w-full"
               />
-              
-              
 
-              <label className="flex items-center text-sm my-8 text-gray-500"><input type="checkbox" name="Remember" className="border-2 rounded border-gray-300 mr-2">
-              </input>I agree to all the <Link href="/"><p className="text-sm font-normal text-blue-500 cursor:pointer mx-1">Terms of conditions</p></Link> &amp; <Link href="/"><p className="text-sm font-normal text-blue-500 cursor:pointer mx-1">Privacy Policy</p></Link></label>     
-              
+              <label className="flex items-center text-sm my-8 text-gray-500">
+                <input
+                  type="checkbox"
+                  name="Remember"
+                  className="border-2 rounded border-gray-300 mr-2"
+                ></input>
+                I agree to all the
+                <Link href="/">
+                  <a className="text-sm font-normal text-blue-500 cursor:pointer mx-1">
+                    Terms of conditions
+                  </a>
+                </Link>
+                &
+                <Link href="/">
+                  <a className="text-sm font-normal text-blue-500 cursor:pointer mx-1">
+                    Privacy Policy
+                  </a>
+                </Link>
+              </label>
+
               <div className="flex mt-4">
                 <button
                   type="submit"
                   className="border-2 border-black-500 rounded text-white bg-black text-xl px-12 py-2 font-normal hover:bg-green-500"
-                > Create Account
+                >
+                  Create Account
                 </button>
               </div>
-              
-              <div className="flex justify-left gap-2 mt-12">           
-                <span className="text-sm font-normal text-black-500 text-left mb-2">Already have an account?</span>
-                <Link href="/"><p className="text-sm font-normal text-blue-500 cursor:pointer">Log In</p></Link>
-              </div>          
+
+              <div className="flex justify-left gap-2 mt-12">
+                <span className="text-sm font-normal text-black-500 text-left mb-2">
+                  Already have an account?
+                </span>
+                <Link href="/auth">
+                  <a className="text-sm font-normal text-blue-500 cursor:pointer">
+                    Log In
+                  </a>
+                </Link>
+              </div>
             </form>
-            
           </div>
           <div className="w-1/2 bg-black text-white p-32">
             <div className="grid grid-col place-content-center w-full items-center gap-8 mt-32">
@@ -88,11 +117,11 @@ export default function Home() {
               </p>
             </div>
             <div className="flex w-full mt-24 mx-auto">
-              <Image src={sidekeyboard}/>
+              <Image src={sidekeyboard} />
             </div>
           </div>
         </div>
       </main>
     </>
-  )
+  );
 }
