1e1849cb8fd9680031b7f951d8446a15953ffdea
David Pamerean Budiarto <davidpamereanb@gmail.com>
2022-04-24 22:55:21 +0700

    feat: split priceinfo into its own component

---
+++ components/cart/PriceInfo.tsx
@@ -0,0 +1,65 @@
+import { useContext } from "react";
+import { CartContext } from "contexts/CartContext";
+// components
+import Image from "next/image";
+import Arrow from "public/images/Arrow.png";
+import Link from "next/link";
+
+type Props = {
+  className?: string;
+};
+
+const PriceInfo = ({ className }: Props) => {
+  const { contents, getTotalPrice, getTotalQuantity } = useContext(CartContext);
+
+  return (
+    <div
+      className={`gap-4 flex flex-col p-8 w-full bg-shark-500 self-start ${className}`}
+    >
+      <div className="flex bg-tuatara-500 rounded border-2 border-colonialwhite-500 ">
+        <h2 className="p-2 text-sm font-basic text-colonialwhite-500">
+          Use Coupon Code or KeyThicc Points
+        </h2>
+        <Image className="m-10" src={Arrow} />
+      </div>
+      <h2 className=" text-2xl font-semibold text-gray-50">Summary</h2>
+      <div className="flex justify-between">
+        <p className=" text-lg font-light text-gray-50">
+          item price ({getTotalQuantity()} items)
+        </p>
+        <p className=" text-lg font-light text-gray-50">
+          Rp{getTotalPrice().toLocaleString()}
+        </p>
+      </div>
+      <div className="flex justify-between">
+        <p className=" text-lg font-light text-manatee-500">
+          Total item discount
+        </p>
+        <p className="text-lg font-light text-manatee-500">-$22.50</p>
+      </div>
+
+      <hr className="w-full bg-manatee-500" />
+
+      <div className="flex justify-between">
+        <p className=" text-lg font-light text-gray-50">Total Price</p>
+        <p className="text-lg font-light text-gray-50">
+          Rp{getTotalPrice().toLocaleString()}
+        </p>
+      </div>
+      <Link href="/cart/checkout">
+        <button
+          className={`text-center text-lg font-semibold text-black w-full p-4  ${
+            contents.length === 0
+              ? "opacity-50 cursor-not-allowed bg-gray-500"
+              : "bg-yellow-500 "
+          }`}
+          disabled={contents.length === 0}
+        >
+          CHECKOUT
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default PriceInfo;
