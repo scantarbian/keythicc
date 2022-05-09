4fd113270939a53a04842388c9e2fea7fbe6bb64
David Pamerean Budiarto <davidpamereanb@gmail.com>
2022-05-09 12:42:11 +0700

    type: response type for midtrans card

--- pages/api/midtrans/notification.tsx
+++ pages/api/midtrans/notification.tsx
@@ -1,17 +1,57 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+type BasicResponse = {
+  status_code: string;
+  status_message: string;
+  transaction_id: string;
+  order_id: string;
+  merchant_id: string;
+  gross_amount: string;
+  currency: string;
+  payment_type: string;
+  transaction_time: string;
+  transaction_status: string;
+  fraud_status: string;
+};
+
+interface CardFailureResponse extends BasicResponse {
+  bank: string;
+  eci: string;
+  masked_card: string;
+}
+
+interface CardSuccessResponse extends BasicResponse {
+  channel_response_code: string;
+  channel_response_message: string;
+  bank: string;
+  eci: string;
+  approval_code: string;
+  masked_card: string;
+  card_type: string;
+  three_ds_version: string;
+  challenge_completion: boolean;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   try {
-    const { method, body } = req;
+    const {
+      method,
+      body,
+    }: {
+      method?: string | undefined;
+      body: CardFailureResponse | CardSuccessResponse;
+    } = req;
 
     switch (method) {
       case "POST": {
-        const callback = JSON.parse(body);
+        console.log(body);
 
-        console.log(callback);
+        return res.status(200).json({
+          status: "success",
+        });
       }
       default: {
         return res.status(405).end();
